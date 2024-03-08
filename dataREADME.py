97db07ea3bbbe27e49435ac655b1c896a9ce95bf
putteneersjoris <putteneersjoris@gmail.com>
2024-03-08 01:37:13 +0100

    update

--- dataREADME.py
+++ dataREADME.py
@@ -1,6 +1,7 @@
 #README content using raw string
 
 admin = "Annelotte"
+mages
 admin_fullname = "Annelotte Lammertse"
 user = "student"
 admin_github_account = "https://github.com/AnnelotteLammertse"
@@ -285,9 +286,9 @@ folder_images = f"""
 
 ### images
 
-Images can be of `.jpg`, `.png`, `.gif`, `.HEIC` format.
-The max image filezize is 10mb.
-the max resolution is 5000x5000 pixels
+1. Images can be of `.jpg`, `.png`, `.gif`, `.HEIC` format.
+2. The max image filezize is 10mb.
+3. the max resolution is 5000x5000 pixels
 
 <div style="display: flex; flex-wrap: wrap;">
     <img src="./example/demoproject/1.jpg" width="32%">
@@ -298,64 +299,72 @@ the max resolution is 5000x5000 pixels
 
 """
 folder_description = f""" 
-### description
-You can only have 1 description. You can upload more but onlt alphabetically first one will be read.
-It can have filenames with spaces, and characters.
-word, or other office documents or any other word processor is not supported. it can only have a .txt file extension. 
 
-Every `.txt` file in every project should have 4 tags. `<title></title>`, `<date>,</date>`, `<body></body>`, `<tags></tags>`:
+### description
+
+1. You can only have 1 description. You can upload more but onlt alphabetically first one will be read.
+2. It can have filenames with spaces, and characters.
+3. word, or other office documents or any other word processor is not supported. it can only have a .txt file extension. 
+4. Every `.txt` file in every project should have 4 tags. `<title></title>`, `<date>,</date>`, `<body></body>`, `<tags></tags>`:
 
 They can be used like this:
-`<title>the title of your project</title> `
-`<date>the date of you project</date>` * this can be in any format you like
-`<tags>textile, bioactive, healthcare, wound healing</tags>` *tags (hashtags) are seperated by a comma `,`
-`<body>
-    the body text of your project.
-    you cna just write text in here. everytime you start a new line, it will appear on the website. symbols, characters and emoji's are supported
-    
-    optionally, it also supports html styling and tags
 
-    <h2>subtitle</h2>
-    <details><summary>dropdown menu</summary>
-        content of a dropdown menu
-    </details>
+```html
 
-    <p>paragraphs can be used to make different sections</p>
+	<title>the title of your project</title> 
+	<date>the date of you project</date>` * this can be in any format you like
+	<tags>textile, bioactive, healthcare, wound healing</tags>` *tags (hashtags) are seperated by a comma `,`
+	<body>
+	the body text of your project.
+	you cna just write text in here. everytime you start a new line, it will appear on the website. symbols, characters and emoji's are supported
 
-    <p style='color:red; text-decoration:underline; background-color:blue'>
-         You can also style every tag by adding color, underline or inline<span style='background-color:blue;'> background color </span>.
-    </p>
-    
-    <a href="https://duckduckgo.com/">internal or internal links are also supported</a>
+	optionally, it also supports html styling and tags
 
-    </body>`
+	<h2>subtitle</h2>
+	<details><summary>dropdown menu</summary>
+	content of a dropdown menu
+	</details>
+
+	<p>paragraphs can be used to make different sections</p>
+
+	<p style='color:red; text-decoration:underline; background-color:blue'>
+	 You can also style every tag by adding color, underline or inline<span style='background-color:blue;'> background color </span>.
+	</p>
+
+	<a href="https://duckduckgo.com/">internal or internal links are also supported</a>
+
+	</body>
+```
 
 """
 folder_description_example = f""" 
-Below is an example of such a `.txt` file
+
+## example description
 
 ```html
-<title>Project 1</title>
-<date>10/10/2024</date>
-<tags>textile, bioactive, healthcare, wound healing</tags>
-<body>
-    <h2>Project Overview</h2>
-    This project focuses on the development of bioactive textiles for applications in wound healing and healthcare. By incorporating bioactive agents into textile fibers, we aim to create functional textiles capable of promoting wound healing, preventing infections, and improving overall healthcare outcomes. The project involves a multidisciplinary approach that combines textile engineering, biomaterials science, and medical research to design innovative solutions for medical textiles.
-    The use of bioactive textiles has the potential to revolutionize wound care by providing continuous, localized delivery of therapeutic agents directly to the wound site. This targeted delivery system minimizes systemic side effects and enhances the efficacy of treatment. Additionally, bioactive textiles offer advantages such as improved patient comfort, reduced dressing changes, and simplified wound management procedures.  
+	<title>Project 1</title>
+	<date>10/10/2024</date>
+	<tags>textile, bioactive, healthcare, wound healing</tags>
+	<body>
+	    <h2>Project Overview</h2>
+	    This project focuses on the development of bioactive textiles for applications in wound healing and healthcare. By incorporating bioactive agents into textile fibers, we aim to create functional textiles capable of promoting wound healing, preventing infections, and improving overall healthcare outcomes. The project involves a multidisciplinary approach that combines textile engineering, biomaterials science, and medical research to design innovative solutions for medical textiles.
+	    The use of bioactive textiles has the potential to revolutionize wound care by providing continuous, localized delivery of therapeutic agents directly to the wound site. This targeted delivery system minimizes systemic side effects and enhances the efficacy of treatment. Additionally, bioactive textiles offer advantages such as improved patient comfort, reduced dressing changes, and simplified wound management procedures.  
 
-    The research objectives of the project include investigating methods for functionalizing textile fibers with bioactive agents, optimizing the release kinetics of therapeutic compounds, and evaluating the biocompatibility and safety of bioactive textiles for clinical use. Advanced fabrication techniques such as electrospinning, coating, and grafting will be employed to incorporate bioactive agents into textile matrices while preserving their structural integrity and mechanical properties.
+	    The research objectives of the project include investigating methods for functionalizing textile fibers with bioactive agents, optimizing the release kinetics of therapeutic compounds, and evaluating the biocompatibility and safety of bioactive textiles for clinical use. Advanced fabrication techniques such as electrospinning, coating, and grafting will be employed to incorporate bioactive agents into textile matrices while preserving their structural integrity and mechanical properties.
 
-    <details><summary>Click for more details</summary>This section contains additional details about the project.
-    <a href="https://www.sciencedirect.com/science/article/pii/S014296121830642X">Read this paper</a>
-    <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5799424/">Explore this study</a>
-    <a href="https://www.frontiersin.org/articles/10.3389/fbioe.2020.587592/full">Find out more</a> about advanced fabrication techniques for bioactive textiles.</details>
-    <details><summary>Click for more details</summary>This section contains additional details about the project.</details>
+	    <details><summary>Click for more details</summary>This section contains additional details about the project.
+	    <a href="https://www.sciencedirect.com/science/article/pii/S014296121830642X">Read this paper</a>
+	    <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5799424/">Explore this study</a>
+	    <a href="https://www.frontiersin.org/articles/10.3389/fbioe.2020.587592/full">Find out more</a> about advanced fabrication techniques for bioactive textiles.</details>
+	    <details><summary>Click for more details</summary>This section contains additional details about the project.</details>
 
-    <p>The expected outcomes of the project include the development of bioactive textiles with tailored properties for specific medical applications, such as wound dressings, compression garments, and implantable devices. These innovative textiles have the potential to improve patient outcomes, reduce healthcare costs, and advance the field of regenerative medicine.</p>
-</body>
+	    <p>The expected outcomes of the project include the development of bioactive textiles with tailored properties for specific medical applications, such as wound dressings, compression garments, and implantable devices. These innovative textiles have the potential to improve patient outcomes, reduce healthcare costs, and advance the field of regenerative medicine.</p>
+	</body>
 ```
 """
-example_visualized = f""" this demoproject will render out on the website like this:
+example_visualized = f""" 
+
+This demoproject will render out on the website like this:
 
 <div style="display: flex; flex-wrap: wrap;">
     <img src="./example/demoproject/demoproject_text.jpg" width="49%">
@@ -372,6 +381,7 @@ demoproject =demoproject_intro + content_tree + folder_tree + folder_images + fo
 
 #navigation
 navigation_overview = f""" 
+
 ## navigation
 
 
@@ -382,19 +392,21 @@ navigation_overview = f"""
 
 #workflow
 workflow = f""" 
+
 ## scripts
 
-every push request activates a github actions protocal  that:
-installs:
-    1.imagemagick for image processing
-    
-generates:
-    1. the static .html webpages for every project.
-    2. the data.js file that is needed for script.js
+Every push request activates a github actions protocal  that:
 
-uploads:
-    script.js
-    index.html
+1. installs:
+    - imagemagick for image processing
+    
+2. generates:
+    - the static .html webpages for every project.
+    - the data.js file that is needed for script.js
+
+3. uploads:
+    - script.js
+    - index.html
 
 """
 
